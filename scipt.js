8652acbe6821de413e74e6d4ca1a26689ea5fd96
Kareem-124 <95405896+Kareem-124@users.noreply.github.com>
2023-04-13 17:49:18 +0300

    upload

---
+++ scipt.js
@@ -0,0 +1,40 @@
+function picUnit(element){
+
+var symbol = element.value;
+console.log(Math.round(0.9));
+console.log(Math.round(69.80000000000001));
+console.log(Math.round(0.9));
+if (symbol != '℃'){
+    var array=document.querySelectorAll(".container .HighLowTemp .high h3");
+    for(var i=0; i<array.length ; i++){
+    console.log(`The coming Value = ${array[i].innerText}`);
+    array[i].innerText = (parseInt(array[i].innerText)*1.8) +32;
+    console.log(`After converstion = ${array[i].innerText}`);
+    array[i].innerText = Math.round(parseInt(array[i].innerText));
+    }
+    var array=document.querySelectorAll(".container .HighLowTemp .low h3");
+    for(var i=0; i<array.length ; i++){
+    console.log(`The coming Value = ${array[i].innerText}`);
+    array[i].innerText = Math.round((parseInt(array[i].innerText)*1.8) +32);
+    }
+    
+}
+else if (symbol == '℃'){
+    var array=document.querySelectorAll(".container .HighLowTemp .high h3");
+    for(var i=0; i<array.length ; i++){
+    console.log(`The coming Value = ${array[i].innerText}`);
+    array[i].innerText = Math.round((parseInt(array[i].innerText)-32 ) *  .556);
+    }
+    var array=document.querySelectorAll(".container .HighLowTemp .low h3");
+    for(var i=0; i<array.length ; i++){
+    console.log(`The coming Value = ${array[i].innerText}`);
+    array[i].innerText = Math.round((parseInt(array[i].innerText)-32 ) *  .556);
+    }
+    
+}
+}
+
+
+function accept(){
+    document.querySelector(".cookies_container").remove();
+}
