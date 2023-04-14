3925c3484aff6dfdc3b2cc16df5dcc26cb88fdcd
Kareem-124 <95405896+Kareem-124@users.noreply.github.com>
2023-04-14 14:21:42 +0300

    update

--- scipt.js
+++ scipt.js
@@ -5,14 +5,11 @@ var symbol = element.value;
 if (symbol != '℃'){
     var array=document.querySelectorAll(".container .HighLowTemp .high h3");
     for(var i=0; i<array.length ; i++){
-    console.log(`The coming Value = ${array[i].innerText}`);
     array[i].innerText = (parseInt(array[i].innerText)*1.8) +32;
-    console.log(`After converstion = ${array[i].innerText}`);
     array[i].innerText = Math.round(parseInt(array[i].innerText));
     }
     var array=document.querySelectorAll(".container .HighLowTemp .low h3");
     for(var i=0; i<array.length ; i++){
-    console.log(`The coming Value = ${array[i].innerText}`);
     array[i].innerText = Math.round((parseInt(array[i].innerText)*1.8) +32);
     }
     
@@ -20,12 +17,10 @@ if (symbol != '℃'){
 else if (symbol == '℃'){
     var array=document.querySelectorAll(".container .HighLowTemp .high h3");
     for(var i=0; i<array.length ; i++){
-    console.log(`The coming Value = ${array[i].innerText}`);
     array[i].innerText = Math.round((parseInt(array[i].innerText)-32 ) *  .556);
     }
     var array=document.querySelectorAll(".container .HighLowTemp .low h3");
     for(var i=0; i<array.length ; i++){
-    console.log(`The coming Value = ${array[i].innerText}`);
     array[i].innerText = Math.round((parseInt(array[i].innerText)-32 ) *  .556);
     }
     
